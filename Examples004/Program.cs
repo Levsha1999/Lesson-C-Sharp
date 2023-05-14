eb894078848060d6ff0eb69a9f7b4ea107df4b9f
IgorK <i.g.o.r.24@mail.ru>
2023-05-14 17:48:00 +1000

    add Example004

---
+++ Examples004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "игорь")
+{
+      Console.WriteLine("Ура, это же Игорь!");
+}
+else
+{
+      Console.Write("Привет, ");
+      Console.WriteLine(username);
+}     
